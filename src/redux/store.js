945aaaa84d90e9ba032d3e1198c51e925f86f06d
SergY29 <yurtin.sergey.v@gmail.com>
2023-01-19 22:05:12 +0200

    work 2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,38 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { createSlice, configureStore, nanoid } from "@reduxjs/toolkit";
 
 const contactSlice = createSlice({
     name: "contacts",
     initialState: [],
     reducers: {
-        addContact(state, action) {
-            state.push(action.payload)
-        },
-        deleteContact(state, action) { },
+        addContact: {
+            reducer(state, action) {
+                state.push(action.payload);
+            }, prepare(name, number) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        name,
+                        number,
+                    },
+                };
+            },
+        }
     },
+    deleteContact(state, action) {
+        const index = state.findIndex(task => task.id === action.payload);
+        state.splice(index, 1);
+    }
 });
 
+console.log(contactSlice)
 
-const { addContact, deleteContact } = contactSlice.actions;
-const contactReducer = contactSlice.reducer;
+export const { addContact, deleteContact } = contactSlice.actions;
+
 
 
 
 export const store = configureStore({
     reducer: {
-        contacts: contactSlice,
+        contacts: contactSlice.reducer,
     }
 });
